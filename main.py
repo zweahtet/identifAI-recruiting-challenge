6ad10da6e6c23302945953ee87610dcee12bc9ac
Zwea Htet <zhtet5@berkeley.edu>
2024-06-27 22:40:01 -0700

    added documentation to fastapi main server endpoints

--- main.py
+++ main.py
@@ -1,42 +1,96 @@
-from fastapi import FastAPI, File, UploadFile, HTTPException
-from pydantic import BaseModel
+from typing import Dict, List
+from fastapi import FastAPI, File, UploadFile, HTTPException, status
+from pydantic import BaseModel, Field
 from io import BytesIO
 from PIL import Image
 import numpy as np
-import cv2
 import uvicorn
-from models.face_analysis import FaceAnalyzer
+from models.image_analyzer import (
+    ImageAnalyzer,
+    Demographics,
+    FacialExpressions,
+    FacialAnalysis,
+)
 
+# Create a FastAPI app
 app = FastAPI()
-analyzer = FaceAnalyzer()
+
+# Initialize the ImageAnalyzer class
+analyzer = ImageAnalyzer()
 
 
 class Profile(BaseModel):
-    description: str
+    demographics: Demographics
+    facial_expressions: FacialExpressions
+    facial_analysis: FacialAnalysis
+
+
+@app.post(
+    "/create-profile",
+    response_model=Profile,
+    description="Create a detailed profile of a person from an image.",
+    summary="Create Profile",
+)
+async def create_profile(file: UploadFile = File(...)) -> Profile:
+    """Create a detailed profile of a person from an image.
+
+    Args:
+        file (UploadFile): The image file containing the person's face.
+
+    Returns:
+        Profile: A Pydantic model containing the detailed profile of the person.
+
+    Raises:
+        HTTPException: If an error occurs during the image analysis.
+    """
+    try:
+        img = Image.open(BytesIO(await file.read()))
+        img_array = np.array(img)
+        profile = analyzer.analyze_face(img_array)
+    except Exception as e:
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=str(e)
+        )
+    return Profile(**profile)
 
 
 class Verification(BaseModel):
-    match: bool
+    match: bool = Field(
+        ..., description="Whether the two images are of the same person."
+    )
 
 
-@app.post("/create-profile", response_model=Profile)
-async def create_profile(file: UploadFile = File(...)):
+@app.post(
+    "/verify-images",
+    response_model=Verification,
+    description="Compare two images to verify if they are of the same person.",
+    summary="Verify Images",
+)
+async def verify_images(
+    file_1: UploadFile = File(...), file_2: UploadFile = File(...)
+) -> Verification:
+    """Compare two images to check if they are of the same person.
+
+    Args:
+        - file_1 (UploadFile): The first image file containing the person's face.
+        - file_2 (UploadFile): The second image file containing the person's face.
+
+    Returns:
+        Verification: A Pydantic model containing the verification result.
+
+    Raises:
+        HTTPException: If an error occurs during the image verification
+    """
     try:
-        img = Image.open(BytesIO(await file.read()))
-        profile_description = analyzer.analyze_face(img)
-        return Profile(description=profile_description)
+        img_1 = Image.open(BytesIO(await file_1.read()))
+        img_2 = Image.open(BytesIO(await file_2.read()))
+        match = analyzer.verify_faces(np.array(img_1), np.array(img_2))
     except Exception as e:
-        return HTTPException(status_code=400, detail=str(e))
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=str(e)
+        )
+    return Verification(match=match)
 
-
-# @app.post("/verify-profile", response_model=Verification)
-# async def verify_profile(file: UploadFile = File(...)):
-#     try:
-#         img = Image.open(BytesIO(await file.read()))
-#         match = analyzer.verify_face(img)
-#         return Verification(match=match)
-#     except Exception as e:
-#         return HTTPException(status_code=400, detail=str(e))
 
 if __name__ == "__main__":
     uvicorn.run(app, host="localhost", port=8000)
